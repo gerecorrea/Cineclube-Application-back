93eff8ee8151455cecb332054e0e0014b1bb1f19
gerecorrea <gere.c@hotmail.com>
2021-07-07 23:10:48 -0300

    Criação comportamento alteração média do filme e quantidade de notas e adição de endPoints enxutos.

--- src/main/java/br/com/cineclube/service/UserMovieRelationService.java
+++ src/main/java/br/com/cineclube/service/UserMovieRelationService.java
@@ -1,5 +1,6 @@
 package br.com.cineclube.service;
 
+import br.com.cineclube.entity.Movie;
 import br.com.cineclube.entity.User;
 import br.com.cineclube.entity.UserMovieRelation;
 import br.com.cineclube.repository.UserMovieRelationRepository;
@@ -18,15 +19,69 @@ public class UserMovieRelationService {
 	@Autowired
 	UserMovieRelationRepository userMovieRelationRepository;
 
+	@Autowired
+	MovieService movieService;
+
 	public UserMovieRelation create(UserMovieRelation userMovieRelation){
+
+		Optional<Movie> movie = movieService.findByUuid(userMovieRelation.getMovieUuid());
+		if (userMovieRelation.getUuid() != null){
+			Optional<UserMovieRelation> userMovieRelation1 = userMovieRelationRepository.findById(userMovieRelation.getUuid());
+			if (movie.isPresent() && userMovieRelation1.isPresent()){
+				if (userMovieRelation1.get().getRating() == 0){
+					// Existe relação, mas não com rating
+					movie.get().setAvgRating((movie.get().getNumVotes() * movie.get().getAvgRating() + userMovieRelation.getRating()) /
+							(movie.get().getNumVotes() + 1));
+					movie.get().setNumVotes(movie.get().getNumVotes() + 1);
+				}
+				else{
+					// Se já existe relação user movie com rating e rating anterior
+					movie.get().setAvgRating( (movie.get().getAvgRating() * movie.get().getNumVotes() - userMovieRelation1.get().getRating()
+							+ userMovieRelation.getRating()) / movie.get().getNumVotes());
+					if (userMovieRelation.getRating() == 0){
+						movie.get().setNumVotes(movie.get().getNumVotes() - 1);
+						userMovieRelation.setRated(false);
+					}
+				}
+			}
+		}else {
+			// Se não existe relação, então criada agora. Só adiciona se rating != 0
+			if (movie.isPresent() && userMovieRelation.getRating() != 0) {
+				movie.get().setAvgRating((movie.get().getNumVotes() * movie.get().getAvgRating() + userMovieRelation.getRating()) /
+						(movie.get().getNumVotes() + 1));
+				movie.get().setNumVotes(movie.get().getNumVotes() + 1);
+			}
+		}
 		return userMovieRelationRepository.save(userMovieRelation);
 	}
+
+	public boolean changeRating(UUID uuid, int rating){
+		UserMovieRelation userMovieRelation = userMovieRelationRepository.findByUuid(uuid);
+		userMovieRelation.setRating(rating);
+		userMovieRelation = create(userMovieRelation);
+		return userMovieRelation.getClass() != null;
+	}
+
+	public boolean changeFavorite(UUID uuid){
+		UserMovieRelation userMovieRelation = userMovieRelationRepository.findByUuid(uuid);
+		userMovieRelation.setFavorite(!userMovieRelation.isFavorite());
+		userMovieRelation = userMovieRelationRepository.save(userMovieRelation);
+		return userMovieRelation.getClass() != null;
+	}
+
+	public boolean changeWatchlist(UUID uuid){
+		UserMovieRelation userMovieRelation = userMovieRelationRepository.findByUuid(uuid);
+		userMovieRelation.setWatchlist(!userMovieRelation.isWatchlist());
+		userMovieRelation = userMovieRelationRepository.save(userMovieRelation);
+		return userMovieRelation.getClass() != null;
+	}
+
 	public void delete(UserMovieRelation userMovieRelation){
 		userMovieRelationRepository.delete(userMovieRelation);
 	}
 
 	public Optional<UserMovieRelation> findByUuid(UUID uuid){
-		return userMovieRelationRepository.findByUuid(uuid);
+		return userMovieRelationRepository.findById(uuid);
 	}
 
 	public Optional<UserMovieRelation> findByUserUuidAndMovieUuid(UUID user, UUID movie){
