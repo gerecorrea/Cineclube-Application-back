00e3b813d58518a19396d5e5fd871afc079c97a8
gerecorrea <gere.c@hotmail.com>
2021-07-14 00:04:23 -0300

    Criação de DTO userMovieRelation + Movie.

---
+++ src/main/java/br/com/cineclube/dto/UserMovieDto.java
@@ -0,0 +1,44 @@
+package br.com.cineclube.dto;
+
+import br.com.cineclube.entity.Movie;
+import br.com.cineclube.entity.UserMovieRelation;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class UserMovieDto {
+
+	private UUID uuid;
+
+	private Movie movie;
+
+	private boolean rated;
+
+	private int rating;
+
+	private boolean watchlist;
+
+	public static UserMovieDto convertToDto(UserMovieRelation userMovieRelation, Optional<Movie> movie) {
+		UserMovieDto userMovieDto = new UserMovieDto();
+
+		if (userMovieRelation.getUuid() != null)
+			userMovieDto.setUuid(userMovieRelation.getUuid());
+
+		if(movie != null && movie.isPresent())
+			userMovieDto.setMovie(movie.get());
+
+		if (userMovieRelation.isRated()){
+			userMovieDto.setRated(true);
+			userMovieDto.setRating(userMovieRelation.getRating());
+		}
+
+		if (userMovieRelation.isWatchlist())
+			userMovieDto.setWatchlist(true);
+
+		return userMovieDto;
+	}
+}
